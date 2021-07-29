6b5e0d11b82036895b1936788fdec82dae7f0aa9
SIEUN WOO <ramtk6726@naver.com>
2021-07-29 17:45:34 +0900

    feat: Implement telephon number and email validation test
    Empty field and Regular Expression.

--- Routes/pv_EnrollWH.js
+++ Routes/pv_EnrollWH.js
@@ -4,6 +4,8 @@ exports.EnrollWH = function (req, res, app, db) {
     connection.connect();
     var onlyNum = /^[0-9]*$/;  // 숫자만 받는 정규식
     var engishDigit = /^[a-zA-Z0-9]+$/;  // 영어 대소문자 및 숫자 받는 정규식
+    var emailReg = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]$/i;
+    var phoneReg = /^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$/;
     var item = {
         warehouseName: req.body.warehouseName,
         warehouseTEL: req.body.warehouseTel,
@@ -34,6 +36,10 @@ exports.EnrollWH = function (req, res, app, db) {
         console.log('errortype4');
     } else if ((engishDigit.test(item.warehouseName) || (engishDigit.test(item.infoComment)) || (engishDigit.test(item.etcComment))) == false) {
         res.send("errortype6");
+    } else if (emailReg.test(item.warehouseEmail) == false) {
+        res.send("errortype9");
+    } else if (phoneReg.test(item.warehouseTEL) == false) {
+        res.send("errortype10");
     } else {
         let reqResult = db.query('SELECT * from RequestForEnroll ORDER BY reqID DESC');
         var reqno = 1;
