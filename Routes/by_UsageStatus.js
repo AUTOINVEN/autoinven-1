45bc5dd933eff0a757484b1dbf4541c917c0ac48
SIEUN WOO <ramtk6726@naver.com>
2021-08-02 14:07:32 +0900

    feat: Show usage data on buyer's usage table

--- Routes/by_UsageStatus.js
+++ Routes/by_UsageStatus.js
@@ -1,21 +1,19 @@
 exports.ContractInfo = function (req, res, app, db) {
     var items = {};
-    var sql = `select * from Contract, Warehouse, Buyer where Warehouse.warehouseID=Contract.warehouseID and buyerID='` + req.session['memberID'] + "'";
+    var sql = `select * from Contract, Warehouse where Warehouse.warehouseID=Contract.warehouseID and buyerID='` + req.session['memberID'] + "'";
     let results = db.query(sql);
     if (results.length > 0) {
         for (var step = 0; step < results.length; step++) {
             results[step].price = results[step].price * results[step].area;
             items[`item${step}`] = {
                 reqID: results[step].reqID,
-                reqDate: results[step].reqDate,
-                reqType: results[step].reqType,
+                buyerID: results[step].buyerID,
                 warehouseID: results[step].warehouseID,
                 warehouseName: results[step].warehouseName,
-                buyerID: results[step].buyerID,
-                area: results[step].area,
-                amounts: results[step].price,
                 startDate: results[step].startDate.substring(0, 10),
-                endDate: results[step].endDate.substring(0, 10)
+                endDate: results[step].endDate.substring(0, 10),
+                area: results[step].area,
+                price: results[step].price
             };
         }
     }
