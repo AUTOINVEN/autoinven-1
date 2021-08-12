16f41e53289b4277d8f0356452e3f02c01f23262
abcworld123 <rkaxhdals@gmail.com>
2021-08-12 11:47:43 +0900

    feat: edit button text, color

--- Public/JS/pv_MyWH.js
+++ Public/JS/pv_MyWH.js
@@ -44,108 +44,110 @@ function reqClick(i, WID) {
 function pvClick(where, i, flag) {
     var text = 'Cancel enrollment?';
     var URL = '/Provider/MyWarehouse/Enroll/Ans';
+    var resTitle = 'Canceled';
     var Area = null;
     if (where) {
-        text = 'Cancel buy request?'
+        text = 'Reject buy request?'
         URL = '/Provider/MyWarehouse/Buy/Ans';
+        resTitle = 'Rejected';
         Area = document.getElementById('area' + where + i).innerText
     }
     switch (flag) {
-        case 0: // Cancel
-            reAlert(text, () => {
-                $.ajax({
-                    url: URL,
-                    dataType: 'json',
-                    type: 'POST',
-                    data: {
-                        answer: "Cancel",
-                        reqID: document.getElementById('reqID' + where + i).innerText,
-                        whID: document.getElementById('whID' + where + i).innerText,
-                        reqType: document.getElementById('reqType' + where + i).innerText,
-                        memberID: document.getElementById('memberID' + where + i).innerText,
-                        area: Area
-                        //other things will be here
-                    },
-                    success: function (data) {
-                        if (data == true) {
-                            Swal.fire({
-                                title: 'Canceled',
-                                icon: 'success'
-                            }).then(() => location.reload());
-                        } else {
-                            Swal.fire({
-                                title: 'Error',
-                                text: 'An error has occurred.',
-                                icon: 'error'
-                            }).then(() => location.reload());
-                        }
+    case 0: // Cancel
+        reAlert(text, () => {
+            $.ajax({
+                url: URL,
+                dataType: 'json',
+                type: 'POST',
+                data: {
+                    answer: "Cancel",
+                    reqID: document.getElementById('reqID' + where + i).innerText,
+                    whID: document.getElementById('whID' + where + i).innerText,
+                    reqType: document.getElementById('reqType' + where + i).innerText,
+                    memberID: document.getElementById('memberID' + where + i).innerText,
+                    area: Area
+                    //other things will be here
+                },
+                success: function (data) {
+                    if (data == true) {
+                        Swal.fire({
+                            title: resTitle,
+                            icon: 'success'
+                        }).then(() => location.reload());
+                    } else {
+                        Swal.fire({
+                            title: 'Error',
+                            text: 'An error has occurred.',
+                            icon: 'error'
+                        }).then(() => location.reload());
                     }
-                });
+                }
             });
-            break;
-        case 1: // Approve
-            reAlert('Approve buy request?', () => {
-                $.ajax({
-                    url: URL,
-                    dataType: 'json',
-                    type: 'POST',
-                    data: {
-                        answer: "Approve",
-                        reqID: document.getElementById('reqID' + where + i).innerText,
-                        whID: document.getElementById('whID' + where + i).innerText,
-                        reqType: document.getElementById('reqType' + where + i).innerText,
-                        memberID: document.getElementById('memberID' + where + i).innerText,
-                        area: Area
-                        //other things will be here
-                    },
-                    success: function (data) {
-                        if (data == true) {
-                            Swal.fire({
-                                title: 'Accepted',
-                                icon: 'success'
-                            }).then(() => location.reload());
-                        } else {
-                            Swal.fire({
-                                title: 'Error',
-                                text: 'An error has occurred.',
-                                icon: 'error'
-                            }).then(() => location.reload());
-                        }
+        });
+        break;
+    case 1: // Approve
+        reAlert('Approve buy request?', () => {
+            $.ajax({
+                url: URL,
+                dataType: 'json',
+                type: 'POST',
+                data: {
+                    answer: "Approve",
+                    reqID: document.getElementById('reqID' + where + i).innerText,
+                    whID: document.getElementById('whID' + where + i).innerText,
+                    reqType: document.getElementById('reqType' + where + i).innerText,
+                    memberID: document.getElementById('memberID' + where + i).innerText,
+                    area: Area
+                    //other things will be here
+                },
+                success: function (data) {
+                    if (data == true) {
+                        Swal.fire({
+                            title: 'Accepted',
+                            icon: 'success'
+                        }).then(() => location.reload());
+                    } else {
+                        Swal.fire({
+                            title: 'Error',
+                            text: 'An error has occurred.',
+                            icon: 'error'
+                        }).then(() => location.reload());
                     }
-                });
+                }
             });
-            break;
-        case 2: // Confirm
-            reAlert('Delete from table?', () => {
-                $.ajax({
-                    url: URL,
-                    dataType: 'json',
-                    type: 'POST',
-                    data: {
-                        answer: "Confirm",
-                        reqID: document.getElementById('reqID' + where + i).innerText,
-                        whID: document.getElementById('whID' + where + i).innerText,
-                        reqType: document.getElementById('reqType' + where + i).innerText,
-                        memberID: document.getElementById('memberID' + where + i).innerText,
-                        area: Area
-                        //other things will be here
-                    },
-                    success: function (data) {
-                        if (data == true) {
-                            Swal.fire({
-                                title: 'Confirmed',
-                                icon: 'success'
-                            }).then(() => location.href = "/Provider/MyWarehouse");
-                        } else {
-                            Swal.fire({
-                                title: 'Error',
-                                text: 'An error has occurred.',
-                                icon: 'error'
-                            }).then(() => location.href = "/Provider/MyWarehouse");
-                        }
+        });
+        break;
+    case 2: // Confirm
+        reAlert('Delete from table?', () => {
+            $.ajax({
+                url: URL,
+                dataType: 'json',
+                type: 'POST',
+                data: {
+                    answer: "Confirm",
+                    reqID: document.getElementById('reqID' + where + i).innerText,
+                    whID: document.getElementById('whID' + where + i).innerText,
+                    reqType: document.getElementById('reqType' + where + i).innerText,
+                    memberID: document.getElementById('memberID' + where + i).innerText,
+                    area: Area
+                    //other things will be here
+                },
+                success: function (data) {
+                    if (data == true) {
+                        Swal.fire({
+                            title: 'Deleted',
+                            icon: 'success'
+                        }).then(() => location.reload());
+                    } else {
+                        Swal.fire({
+                            title: 'Error',
+                            text: 'An error has occurred.',
+                            icon: 'error'
+                        }).then(() => location.reload());
                     }
-                });
+                }
             });
-            break;
+        });
+        break;
     }
 }
