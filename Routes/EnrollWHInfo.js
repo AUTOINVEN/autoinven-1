c6d430fd1ce4ceabdce7f5993846d6060cedcdc4
SIEUN WOO <ramtk6726@naver.com>
2021-08-17 06:01:08 +0000

    feat: Show WH data in Request Enroll (Admin)

--- Routes/EnrollWHInfo.js
+++ Routes/EnrollWHInfo.js
@@ -8,12 +8,8 @@ exports.getWHInfo = function (req, res, app, db) {
 }
 
 exports.getPVInfo = function (req, res, app, db) {
-    var warehouseID = req.body.warehouseID;
+    var providerID = req.body.providerID;
     var items = {};
-    let results = db.query(`select * from Warehouse, Provider where Warehouse.warehouseID=Provider.warehouseID and Warehouse.warehouseID=` + warehouseID + `;`);
-    if (results.length > 0) {
-        var providerID = results[0].memberID;
-        items = viewInfo.getMemberInfo(db, providerID);
-    }
+    items = viewInfo.getMemberInfo(db, providerID);
     return items;
 }
