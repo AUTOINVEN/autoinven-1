dbc4f340ac76dec7f4daa1d39080dbc4ff8da204
SIEUN WOO <ramtk6726@naver.com>
2021-08-19 01:43:16 +0000

    feat: Add validation check in IoT Edit page

--- Routes/iot_EditItem.js
+++ Routes/iot_EditItem.js
@@ -1,14 +1,14 @@
 exports.editItem = function (req, res, db) {
-    var rfid = req.body.editItem_rfid;
-    var name = req.body.editItem_name;
-    var num = req.body.editItem_num;
+    var rfid = req.body.RFID;
+    var name = req.body.name;
+    var num = req.body.num;
 
     var editSQL = `UPDATE iot SET name='${name}',num='${num}' WHERE rfid='${rfid}'`
     var check = db.query(editSQL);
     if (!check) {
-        console.log("error ocurred", error);
-        res.redirect('Warehousing');
+        console.log("error ocurred", error);        
+        res.send("error")
     } else {
-        res.redirect('Warehousing');
+        res.send("success")
     }
 }
--- Routes/iot_RegisterItem.js
+++ Routes/iot_RegisterItem.js
@@ -1,13 +1,13 @@
 exports.registerItem = function (req, res, db) {
-    var rfid = req.body.rfid.toUpperCase();
+    var RFID = req.body.RFID.toUpperCase();
     var name = req.body.name;
     var num = req.body.num;
     var received = 0;
-    var picture = `./${rfid}.jpg`;
+    var picture = `./${RFID}.jpg`;
     var id = req.session['memberID'];
     var wid = req.session['warehouseID'];
 
-    var row = db.query(`INSERT INTO iot VALUES('${rfid}', '${id}', '${name}', ${num}, ${received}, '${picture}',${wid});`);
+    var row = db.query(`INSERT INTO iot VALUES('${RFID}', '${id}', '${name}', ${num}, ${received}, '${picture}',${wid});`);
     if (!row) console.log('err: registerItem');
     else res.redirect('Warehousing');
 }
